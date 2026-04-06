6cc0e1371d0b68f2dd7b407a97c70f1a044436d0
Sidarth Roy <sidarthroy@yahoo.com>
2026-04-06 22:14:59 +0530

    split OrderUpsertReq btw OrderInsertReq and OrderUpdateReq. updated OrderDetailResponse to include names to ID as well

--- Backend/internal/dto/order_dto.go
+++ Backend/internal/dto/order_dto.go
@@ -1,6 +1,6 @@
 package dto
 
-import "time"
+// import "time"
 
 type OrderDetailReq struct {
 	ProductID uint    `json:"productID" binding:"required"`
@@ -9,35 +9,62 @@ type OrderDetailReq struct {
 	Discount  float64 `json:"discount"`
 }
 
-type OrderUpsertReq struct {
+// type OrderUpsertReq struct {
+// 	CustomerID   string           `json:"customerID" binding:"required"`
+// 	EmployeeID   uint             `json:"employeeID" binding:"required"`
+// 	OrderDate    time.Time        `json:"orderDate" binding:"required"`
+// 	RequiredDate time.Time        `json:"requiredDate" binding:"required"`
+// 	ShippedDate  *time.Time       `json:"shippedDate"`
+// 	ShipperID    uint             `json:"shipperID" binding:"required"`
+// 	Freight      float64          `json:"freight" binding:"gte=0"`
+// 	OrderDetails []OrderDetailReq `json:"orderDetails" binding:"required,dive,required"` // 'dive' validates the nested slice
+// }
+
+type OrderInsertReq struct {
+	OrderID	  uint             `json:"orderID" binding:"required"`
 	CustomerID   string           `json:"customerID" binding:"required"`
 	EmployeeID   uint             `json:"employeeID" binding:"required"`
-	OrderDate    time.Time        `json:"orderDate" binding:"required"`
-	RequiredDate time.Time        `json:"requiredDate" binding:"required"`
-	ShippedDate  *time.Time       `json:"shippedDate"`
+	OrderDate    string        `json:"orderDate" binding:"required"`
+	RequiredDate string        `json:"requiredDate" binding:"required"`
+	ShippedDate   string       `json:"shippedDate"`
+	ShipperID    uint             `json:"shipperID" binding:"required"`
+	Freight      float64          `json:"freight" binding:"gte=0"`
+	OrderDetails []OrderDetailReq `json:"orderDetails" binding:"required,dive,required"` // 'dive' validates the nested slice
+}
+
+type OrderUpdateReq struct {
+	CustomerID   string           `json:"customerID" binding:"required"`
+	EmployeeID   uint             `json:"employeeID" binding:"required"`
+	OrderDate    string        `json:"orderDate" binding:"required"`
+	RequiredDate string        `json:"requiredDate" binding:"required"`
+	ShippedDate   string       `json:"shippedDate"`
 	ShipperID    uint             `json:"shipperID" binding:"required"`
 	Freight      float64          `json:"freight" binding:"gte=0"`
 	OrderDetails []OrderDetailReq `json:"orderDetails" binding:"required,dive,required"` // 'dive' validates the nested slice
 }
 
 type OrderDetailResponse struct {
-	ID        uint    `json:"id"`
-	ProductID uint    `json:"productID"`
-	UnitPrice float64 `json:"unitPrice"`
-	Quantity  int     `json:"quantity"`
-	Discount  float64 `json:"discount"`
+	ID			uint    `json:"id"`
+	ProductID	uint    `json:"productID"`
+	ProductName string  `json:"productName"`
+	UnitPrice 	float64 `json:"unitPrice"`
+	Quantity  	int     `json:"quantity"`
+	Discount  	float64 `json:"discount"`
 }
 
 type OrderResponse struct {
 	ID           uint                  `json:"id"`
 	CustomerID   string                `json:"customerID"`
+	CustomerName string                `json:"customerName"`
 	EmployeeID   uint                  `json:"employeeID"`
-	OrderDate    time.Time             `json:"orderDate"`
-	RequiredDate time.Time             `json:"requiredDate"`
-	ShippedDate  *time.Time            `json:"shippedDate"`
+	EmployeeName string                `json:"employeeName"`
+	OrderDate    string	               `json:"orderDate"`
+	RequiredDate string	               `json:"requiredDate"`
+	ShippedDate  string	               `json:"shippedDate"`
 	ShipperID    uint                  `json:"shipperID"`
+	ShipperName  string                `json:"shipperName"`
 	Freight      float64               `json:"freight"`
 	Active       bool                  `json:"active"`
-	ModifiedAt   string                `json:"modifiedAt"`
+	// ModifiedAt   string                `json:"modifiedAt"`
 	OrderDetails []OrderDetailResponse `json:"orderDetails"`
 }
