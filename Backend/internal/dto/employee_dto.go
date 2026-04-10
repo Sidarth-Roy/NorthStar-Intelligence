d61e8b9c94e071001cf0a8b57d4006056c5cce4a
Sidarth Roy <sidarthroy@yahoo.com>
2026-04-11 00:13:13 +0530

    added new dtos EmployeeInsertReq and EmployeeUpdateReq to replace EmployeeUpsertReq for seperation of interest

--- Backend/internal/dto/employee_dto.go
+++ Backend/internal/dto/employee_dto.go
@@ -1,6 +1,22 @@
 package dto
 
-type EmployeeUpsertReq struct {
+// type EmployeeUpsertReq struct {
+// 	EmployeeName string `json:"employeeName" binding:"required,min=3"`
+// 	Title        string `json:"title" binding:"required"`
+// 	City         string `json:"city"`
+// 	Country      string `json:"country"`
+// 	ReportsTo    *uint  `json:"reportsTo"` // Pointer allows null in JSON
+// }
+
+type EmployeeInsertReq struct {
+	EmployeeName string `json:"employeeName" binding:"required,min=3"`
+	Title        string `json:"title" binding:"required"`
+	City         string `json:"city"`
+	Country      string `json:"country"`
+	ReportsTo    *uint  `json:"reportsTo"` // Pointer allows null in JSON
+}
+
+type EmployeeUpdateReq struct {
 	EmployeeName string `json:"employeeName" binding:"required,min=3"`
 	Title        string `json:"title" binding:"required"`
 	City         string `json:"city"`
@@ -9,12 +25,12 @@ type EmployeeUpsertReq struct {
 }
 
 type EmployeeResponse struct {
-	ID           uint   `json:"id"`
-	EmployeeName string `json:"employeeName"`
-	Title        string `json:"title"`
-	City         string `json:"city"`
-	Country      string `json:"country"`
-	ReportsTo    *uint  `json:"reportsTo"`
-	Active       bool   `json:"active"`
-	ModifiedAt   string `json:"modifiedAt"`
+	ID           	uint   	`json:"id"`
+	EmployeeName 	string 	`json:"employeeName"`
+	Title        	string 	`json:"title"`
+	City         	string 	`json:"city"`
+	Country      	string 	`json:"country"`
+	ReportsTo    	*uint  	`json:"reportsTo"`
+	Active       	bool   	`json:"active"`
+	// ModifiedAt   string `json:"modifiedAt"`
 }
