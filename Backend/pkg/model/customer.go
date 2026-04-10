e47e1c1e6d64fd778d91f0289441a087aa622f1a
Sidarth Roy <sidarthroy@yahoo.com>
2026-04-10 22:27:14 +0530

    updated customer model  to keep CustomerID and CompanyName be unique columns

--- Backend/pkg/model/customer.go
+++ Backend/pkg/model/customer.go
@@ -2,8 +2,8 @@ package model
 
 type Customer struct {
 	Base
-	CustomerID   string  `gorm:"uniqueIndex" json:"customerID"` // The string ID from CSV
-	CompanyName  string  `json:"companyName"`
+	CustomerID   string  `gorm:"unique" json:"customerID"` // The string ID from CSV
+	CompanyName  string  `gorm:"unique" json:"companyName"`
 	ContactName  string  `json:"contactName"`
 	ContactTitle string  `json:"contactTitle"`
 	City         string  `json:"city"`
