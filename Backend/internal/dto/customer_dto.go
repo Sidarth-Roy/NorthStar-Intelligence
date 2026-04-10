9b65353261c881b3af57334c353da2deeda6d114
Sidarth Roy <sidarthroy@yahoo.com>
2026-04-10 22:23:15 +0530

    replace dto CustomerUpsertReq to CustomerInsertReq and CustomerUpdateReq for seperation of logic

--- Backend/internal/dto/customer_dto.go
+++ Backend/internal/dto/customer_dto.go
@@ -1,12 +1,32 @@
 package dto
 
-type CustomerUpsertReq struct {
+// type CustomerUpsertReq struct {
+// 	CustomerID   string `json:"customerID" binding:"required,min=5,max=5"` // Specific for Northwind IDs
+// 	CompanyName  string `json:"companyName" binding:"required,min=3"`
+// 	ContactName  string `json:"contactName"`
+// 	ContactTitle string `json:"contactTitle"`
+// 	City         string `json:"city"`
+// 	Country      string `json:"country"`
+// }
+
+type CustomerInsertReq struct {
 	CustomerID   string `json:"customerID" binding:"required,min=5,max=5"` // Specific for Northwind IDs
 	CompanyName  string `json:"companyName" binding:"required,min=3"`
 	ContactName  string `json:"contactName"`
 	ContactTitle string `json:"contactTitle"`
 	City         string `json:"city"`
 	Country      string `json:"country"`
+	Active       bool   `json:"active"`
+}
+
+type CustomerUpdateReq struct {
+	CustomerID   string `json:"customerID" binding:"required,min=5,max=5"` // Specific for Northwind IDs
+	CompanyName  string `json:"companyName" binding:"required,min=3"`
+	ContactName  string `json:"contactName" binding:"required"`
+	ContactTitle string `json:"contactTitle" binding:"required"`
+	City         string `json:"city" binding:"required"`
+	Country      string `json:"country" binding:"required"`
+	Active       bool   `json:"active"`
 }
 
 type CustomerResponse struct {
